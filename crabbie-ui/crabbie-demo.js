317e486187f4eeaf7aee8b874bc3f7bd0e83bb12
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-04-13 12:47:37 -0700

    Create crabbie-demo.js

---
+++ crabbie-ui/crabbie-demo.js
@@ -0,0 +1,17 @@
+var CrabbieAI = {
+    GetResponseGPT35: function (messages, onchunk) {
+        onchunk({
+            chunk: 'This is a demo of the UI design for Crabbie. You are not actually interfacing with a real AI. It also supports `inline code blocks`, ```\nmulti-line code\ncode\nblocks```, **bold**, *italic*, and ~~strikethrough~~ via Markdown, and also Crabmojis %smile% using Crabmoji Syntax `%crabmojiname%`'
+        });
+    },
+    GetResponseGPT4: function (messages, onchunk) {
+        onchunk({
+            chunk: 'This is a demo of the UI design for Crabbie. You are not actually interfacing with a real AI. It also supports `inline code blocks`, ```\nmulti-line code\ncode\nblocks```, **bold**, *italic*, and ~~strikethrough~~ via Markdown, and also Crabmojis %smile% using Crabmoji Syntax `%crabmojiname%`'
+        });
+    },
+    GetResponseGPT4_32k: function (messages, onchunk) {
+        onchunk({
+            chunk: 'This is a demo of the UI design for Crabbie. You are not actually interfacing with a real AI. It also supports `inline code blocks`, ```\nmulti-line code\ncode\nblocks```, **bold**, *italic*, and ~~strikethrough~~ via Markdown, and also Crabmojis %smile% using Crabmoji Syntax `%crabmojiname%`'
+        });
+    }
+}
